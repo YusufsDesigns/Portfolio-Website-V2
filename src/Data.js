7aa7d2b534a453a88ea4f59d9b729706abaec0e4
Yusuf Lawal <yusuf.olaoluwalawal@outlook.com>
2023-06-19 00:08:54 +0100

    New Commit

--- src/Data.js
+++ src/Data.js
@@ -4,7 +4,7 @@ export const data = [
         description: 'A dynamic social media web application designed to connect users, foster engagement, with features such as posts, messages, channels and much more.',
         projectLink: 'https://socialite-yusufsdesigns.vercel.app/',
         codeLink: 'https://github.com/YusufsDesigns/sociaLite',
-        imgUrl: 'https://cdn.sanity.io/images/6c2m2nyc/production/fcee02f52c3f9b08e2e47c7887a19a1a30a5de42-3008x1693.png',
+        imgUrl: 'https://cdn.sanity.io/images/6c2m2nyc/production/41eee1cdf1e88db936d25afcc1212d2e6988e9aa-3008x1534.png',
         tags: ['Web App', 'React JS', 'All'],
         stacks: ['React JS', 'Tailwind CSS', 'Context API', 'Firebase', 'Git', 'Vercel'],
     },
@@ -13,7 +13,7 @@ export const data = [
         description: 'A music player web application that allows users to stream audio and search for their favorite song or artist.',
         projectLink: 'https://winamp-music-app.vercel.app/',
         codeLink: 'https://github.com/YusufsDesigns/WinAmp-Music-App',
-        imgUrl: 'https://cdn.sanity.io/images/6c2m2nyc/production/1c1f8321fa4637d1084b128a7f1004c5b5307b82-3008x1692.png',
+        imgUrl: 'https://cdn.sanity.io/images/6c2m2nyc/production/1367265645508aed6320cb2b402d1580dcd0cb49-3008x1692.png',
         tags: ['Web App', 'React JS', 'All'],
         stacks: ['React JS', 'Tailwind CSS', 'Deezer API', 'Git', 'Vercel']
     },
@@ -22,7 +22,7 @@ export const data = [
         description: 'A chat application that allows users to connect and chat effortlessly with friends, and family, share messages, photos, and stay connected wherever you go.',
         projectLink: 'https://chat-app-yusufsdesigns.vercel.app/',
         codeLink: 'https://github.com/YusufsDesigns/Chat-App',
-        imgUrl: 'https://cdn.sanity.io/images/6c2m2nyc/production/a99abaf8c688439494737589c7dcc40e38818300-2820x1692.png',
+        imgUrl: 'https://cdn.sanity.io/images/6c2m2nyc/production/d53f0b5a40c773f7d82c6c9ff06ae2f444b9e53e-2766x1692.png',
         tags: ['Web App', 'React JS', 'All'],
         stacks: ['React JS', 'SCSS', 'Firebase', 'Git', 'Vercel']
     },
@@ -31,7 +31,7 @@ export const data = [
         description: 'Created a responsive home rentals landing page that has pagination and form validation',
         projectLink: 'https://home-rentals-landing-page.vercel.app/',
         codeLink: 'https://github.com/YusufsDesigns/Home-Rentals-Landing-Page',
-        imgUrl: 'https://cdn.sanity.io/images/6c2m2nyc/production/20d1a14b22c5ad3f6caa4cd896915856f928ba83-3008x1576.png',
+        imgUrl: 'https://cdn.sanity.io/images/6c2m2nyc/production/63ec9cb5e04750e743d4365eff18fc6f50057547-3008x1576.png',
         tags: ['React JS', 'All'],
         stacks: ['React JS', 'CSS', 'React-Paginate', 'Git', 'Vercel']
     },
@@ -40,7 +40,7 @@ export const data = [
         description: 'Built a weather application that displays the weather conditions of over 200,000 cities with a change of background depending on the weather condition and time of day of the particular city.',
         projectLink: 'https://weather-app-yusufsdesigns.vercel.app/',
         codeLink: 'https://github.com/YusufsDesigns/Weather-App',
-        imgUrl: 'https://cdn.sanity.io/images/6c2m2nyc/production/b365d0bb6cadc4dbc3dd4f1bbec615342d0bb475-3008x1692.png',
+        imgUrl: 'https://cdn.sanity.io/images/6c2m2nyc/production/825e78329cd9b9486f2ed2d7eabad4caf3fa181c-3008x1692.png',
         tags: ['Web App', 'All'],
         stacks: ['HTML', 'CSS', 'JavaScript', 'WeatherAPI', 'Git', 'Vercel']
     },
@@ -49,7 +49,7 @@ export const data = [
         description: 'A quiz app that tests the knowledge of a person through complex trivia questions and grades them at the end.',
         projectLink: 'https://quizzical-lemon.vercel.app/',
         codeLink: 'https://github.com/YusufsDesigns/Quizzical',
-        imgUrl: 'https://cdn.sanity.io/images/6c2m2nyc/production/7b2a20fe96b43e8bcdc90c2140adacf0092c26ff-2808x1692.png',
+        imgUrl: 'https://cdn.sanity.io/images/6c2m2nyc/production/cefedb4c55b5fc19b45c0b5a2dab246983817821-2808x1692.png',
         tags: ['Web App', 'React JS', 'All'],
         stacks: ['React JS', 'CSS', 'Open Trivia DB', 'Git', 'Vercel']
     },
@@ -58,7 +58,7 @@ export const data = [
         description: 'I built a game that allows users to roll a die and click each die to freeze it at its current value between rolls until all dice are the same.',
         projectLink: 'https://tenzies-amber.vercel.app/',
         codeLink: 'https://github.com/YusufsDesigns/Tenzies',
-        imgUrl: 'https://cdn.sanity.io/images/6c2m2nyc/production/986b51b2e93f33ffd449eb98830a727193cbf718-2808x1692.png',
+        imgUrl: 'https://cdn.sanity.io/images/6c2m2nyc/production/6c0082437f27f6afca18f0ad462d2640f20bae0e-2808x1692.png',
         tags: ['Web App', 'React JS', 'All'],
         stacks: ['React JS', 'CSS', 'Git', 'Vercel']
     },
